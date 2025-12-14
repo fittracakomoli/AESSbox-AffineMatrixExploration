42782b6bddd22271e47e78426f2816250aa48076
Fittra Marga Ardana <fittracakomoli6@gmail.com>
2025-12-15 06:21:02 +0700

    refactor/main move main from app to root

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ app.add_middleware(
 app.include_router(api_router)
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
